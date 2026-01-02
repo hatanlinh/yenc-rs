e1c15b4bae66b3278eedb57e0fd38053c1cedab1
hatanlinh13 <hatanlinh13@gmail.com>
2026-01-02 20:37:27 +0700

    Some useless code

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,18 @@
-use std::path::PathBuf;
+use std::{
+    fs::OpenOptions,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
+
+fn parse_header_line(_line: &str) -> bool {
+    // TODO: Implement parse header line
+    return true;
+}
+
+fn decode_line(_line: &str) -> bool {
+    // TODO: Implement decode line
+    return true;
+}
 
 pub fn decode(input: PathBuf, output: PathBuf, debug_mode: bool) -> bool {
     if debug_mode {
@@ -7,6 +21,23 @@ pub fn decode(input: PathBuf, output: PathBuf, debug_mode: bool) -> bool {
             input.to_string_lossy(),
             output.to_string_lossy()
         )
+    }
+
+    let input_file = OpenOptions::new()
+        .read(true)
+        .open(input)
+        .unwrap_or_else(|_| panic!("Invalid input!"));
+    let input_reader = BufReader::new(input_file);
+
+    let mut yenc_start_found = false;
+    for line in input_reader.lines() {
+        let line = line.unwrap();
+        if line.starts_with("=ybegin ") {
+            yenc_start_found = true;
+            parse_header_line(&line);
+        } else if yenc_start_found {
+            decode_line(&&line);
+        }
     }
 
     true
